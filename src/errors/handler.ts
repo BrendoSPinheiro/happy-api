f94549cd9a24f993e93579c128a2bda2540c25b8
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-02-07 22:40:47 -0300

    feat: :sparkles: update rules in eslint

--- src/errors/handler.ts
+++ src/errors/handler.ts
@@ -1,4 +1,4 @@
-import { ErrorRequestHandler } from 'express'
+import { ErrorRequestHandler } from 'express';
 import { ValidationError } from 'yup';
 
 interface ValidationErrors{
@@ -6,20 +6,17 @@ interface ValidationErrors{
 }
 
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  if (error instanceof ValidationError) {
+    const errors: ValidationErrors = {};
 
-  if(error instanceof ValidationError){
-    let errors: ValidationErrors = {};
-
-    error.inner.forEach(err => {
+    error.inner.forEach((err) => {
       errors[err.path] = err.errors;
-    })
+    });
 
     return res.status(400).json({ message: 'Validation fails', errors });
   }
 
-  console.error(error);
-
   return res.status(500).json({ message: 'Internal server error' });
-}
+};
 
 export default errorHandler;
